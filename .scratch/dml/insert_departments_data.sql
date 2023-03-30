f60a413c273f14cc9a068ccf14a71c745cb1aaf1
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-03-30 16:00:52 -0400

    dml

---
+++ .scratch/dml/insert_departments_data.sql
@@ -0,0 +1,14 @@
+insert into departments ( id, name, location, country) values (1,'Budget Compliance','Tanquecitos','United States');
+insert into departments (id,name,location,country) values (2,'Operations','Sugarloaf','United States');
+insert into departments (id,name,location,country) values (3,'Data Lifecycle Management','Dale City','United States');
+insert into departments (
+    id,
+    name,
+    location,
+    country
+) values (
+    4,
+    'Internal Systems',
+    'Grosvenor',
+    'United States'
+);
