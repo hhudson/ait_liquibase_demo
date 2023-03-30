3dcb48a0bdf45a7576d5b8d237552d2ef6563b87
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-03-30 16:01:42 -0400

    data

--- templates/data.sql
+++ templates/data.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 --changeset ait:CHANGEME_data stripComments:false splitStatements="true" stripComments:false
-create or replace force view CHANGEME as
-  
+
+insert into CHANGEME ...
 ;
