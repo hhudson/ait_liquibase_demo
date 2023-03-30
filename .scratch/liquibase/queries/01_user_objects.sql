ae60b9e6958d4a34dd87a246b82962e4cf9fcb42
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-03-30 15:12:57 -0400

    other folders

--- .scratch/liquibase/queries/01_user_objects.sql
+++ .scratch/liquibase/queries/01_user_objects.sql
@@ -1,5 +1,6 @@
 select object_type, object_name
 from user_objects
 where object_type in ('PACKAGE', 'PACKAGE BODY', 'TABLE', 'VIEW', 'TRIGGER')
+and object_name not like 'DATABASECHANGELOG%'
 order by 1,2
 /
